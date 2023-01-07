513e1f73477e21de5dc97a08845f4347bd8be593
EthanAmato <ethannamato@gmail.com>
2023-01-07 11:27:32 -0500

    added cube animation to about page and better constrasting colors to home page

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,16 +1,107 @@
 import AnimatedLetters from '../AnimatedLetters';
 import { useState, useEffect } from 'react';
+import $ from 'jquery';
 import './index.scss';
+import {
+    faAngular,
+    faCss3,
+    faGitAlt,
+    faHtml5,
+    faJsSquare,
+    faReact,
+    faJava,
+    faPython,
+    faRProject,
+    faNode,
+    faSass,
+    faGithub
+} from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
+const icons1 = {
+    name: "icons1",
+    icons: [
+        {
+            icon: faAngular,
+            color: "#DD0031"
+        },
+        {
+            icon: faCss3,
+            color: "#28A4D9"
+        },
+        {
+            icon: faGitAlt,
+            color: "#EC4D28"
+        },
+        {
+            icon: faHtml5,
+            color: "#F06529"
+        },
+        {
+            icon: faJsSquare,
+            color: "#EFD81D"
+        },
+        {
+            icon: faReact,
+            color: "#5ED4F4"
+        },
+    ]
+}
+
+
+const icons2 = {
+    name: "icons2",
+    icons: [
+        {
+            icon: faJava,
+            color: "#447A9F"
+        },
+        {
+            icon: faPython,
+            color: "#FFDB4D"
+        },
+        {
+            icon: faRProject,
+            color: "#1F6ABF"
+        },
+        {
+            icon: faGithub,
+            color: "#000000"
+        },
+        {
+            icon: faNode,
+            color: "#3E863D"
+        },
+        {
+            icon: faSass,
+            color: "#CF649A"
+        },
+    ]
+}
 const About = () => {
     const [letterClass, setLetterClass] = useState('text-animate');
+    const [isIconSet1, setIsIconSet1] = useState(true);
+
+
 
     useEffect(() => {
         setTimeout(() => {
-          setLetterClass('text-animate-hover')
+            setLetterClass('text-animate-hover')
         }, 3000)
-      }, [])
-    
+    }, [])
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setIsIconSet1(!isIconSet1)
+            $(".icon-set-1 div").toggleClass("noOpacity")
+            $(".icon-set-2 div").toggleClass("noOpacity")
+         
+        }, 8000)
+        return () => {
+            clearInterval(interval);
+        };
+    }, [isIconSet1])
+
 
 
     return (
@@ -29,7 +120,7 @@ const About = () => {
                     provide an environment where I can continuously learn and grow
                 </p>
                 <p>
-                    I am naturally inquisitive, inspired by and open to differing perspectives, and 
+                    I am naturally inquisitive, inspired by and open to differing perspectives, and
                     am in a constant cycle of improving my skills
                 </p>
                 <p>
@@ -38,6 +129,32 @@ const About = () => {
                     doing Language Exchange with my friends in Tokyo, or when I'm behind a computer ready to start
                     a new project."
                 </p>
+            </div>
+            <div className="stage-cube-content">
+                    <div className="cube-spinner icon-set-1">
+                        {
+                            icons1.icons.map((iconObject, index) => {
+                                return (
+                                    //cubes aren't 0-indexed (typically)
+                                    <div key={iconObject.icon.iconName + index} className={`face${index + 1}`}>
+                                        <FontAwesomeIcon className='iconSVG' icon={iconObject.icon} color={iconObject.color} />
+                                    </div>
+                                )
+                            })
+                        }
+                    </div>
+                    <div className="cube-spinner icon-set-2">
+                        {
+                            icons2.icons.map((iconObject, index) => {
+                                return (
+                                    //cubes aren't 0-indexed (typically)
+                                    <div key={iconObject.icon.iconName + index} className={`face${index + 1}  noOpacity`}>
+                                        <FontAwesomeIcon className='iconSVG' icon={iconObject.icon} color={iconObject.color} />
+                                    </div>
+                                )
+                            })
+                        }
+                    </div>
             </div>
         </div>
     )
