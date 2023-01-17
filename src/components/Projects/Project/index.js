12286390e7ad8e542a8c666b09e1baa117f4931f
EthanAmato <ethannamato@gmail.com>
2023-01-17 11:19:26 -0500

    added cropped images to carousel, fixed styling, integrated links to respective repositories and deployments, and wrote summaries for each project

--- src/components/Projects/Project/index.js
+++ src/components/Projects/Project/index.js
@@ -1,26 +1,101 @@
 import './index.scss';
+import gsap from 'gsap-trial/dist/gsap';
 const Project = ({
     description,
     image1,
     image2,
     headline,
-    repoURL = "#",
+    repoURL,
     isDeployed = false,
-    deploymentURL = "#" }) => {
+    deploymentURL = ""}) => {
 
+    function handleHover() {
+        let timeline = gsap.timeline();
+        timeline.fromTo(".react-multi-carousel-item--active .image1",
+            {
+                opacity: 1,
+            },
+            {
+                opacity: 0,
+                perspective: "800px",
+                rotateY: "25deg",
+                duration: ".5s"
+            })
+            .fromTo(".react-multi-carousel-item--active .image2", {
+                opacity: 0
+            },
+                {
+                    opacity: 1,
+                    perspective: "800px",
+                    rotateY: "0deg",
+                    duration: "1.5s",
+                })
+        return () => {
+            timeline.clear()
+        }
+    }
 
+    function handleMouseLeave() {
+        let timeline = gsap.timeline();
+        timeline.fromTo(".react-multi-carousel-item--active .image2",
+            {
+                opacity: 1,
+                perspective: "800px",
+                rotateY: "0deg",
+            },
+            {
+                opacity: 0,
+                perspective: "800px",
+                rotateY: "25deg",
+                duration: ".5s",
+                delay: "5s"
+            })
+            .fromTo(".react-multi-carousel-item--active .image1", {
+                opacity: 0,
+                perspective: "800px",
+                rotateY: "0deg",
+            },
+                {
+                    opacity: 1,
+                    perspective: "800px",
+                    rotateY: "0deg",
+                    duration: "1.5s",
+                    delay: "5s"
+                })
+    }
+
+    console.table( description,
+        image1,
+        image2,
+        headline,
+        repoURL,
+        isDeployed,
+        deploymentURL)
     return (
-        <div className='project-wrapper'>
-            <a href="#" className='image1-link'><img draggable={false} className="website-image image1" src={image1} alt={`${headline}`} /></a>
-            <a href="#" className='image2-link opacityNone displayNone'><img draggable={false} className="website-image image2 opacityNone displayNone" src={image2} alt={`${headline}`} /></a>
+        <div className='project-wrapper' onMouseEnter={handleHover} onMouseLeave={handleMouseLeave}>
+            <a href={repoURL} className='image1-link' target="_blank" >
+                <img
+                    draggable={false}
+                    className="website-image image1"
+                    src={image1}
+                    alt={`${headline}`}
+                />
+                <img
+                    draggable={false}
+                    className="website-image image2"
+                    src={image2}
+                    alt={`${headline}`}
+                    style={{ opacity: "0" }}
+                />
+            </a>
             <div className='description-container'>
                 <div className='info'>
                     <h2>{headline}</h2>
                     <p>{description}</p>
                 </div>
                 <div className='buttons'>
-                    {isDeployed ? <a className='button-link' href={deploymentURL}><button>Go To Website</button></a> : <></>}
-                    <a className='button-link' href={repoURL}><button>Go To Repository</button></a>
+                    {isDeployed ? <a href={deploymentURL} target="_blank"><button className='flat-button'>Go To Website</button></a> : <></>}
+                    <a href={repoURL} target="_blank"><button className='flat-button'>Go To Repository</button></a>
                 </div>
             </div>
         </div>
--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -2,124 +2,106 @@ import './index.scss';
 import Project from './Project';
 import Carousel from 'react-multi-carousel';
 import "react-multi-carousel/lib/styles.css"
-import $ from 'jquery'
-import { useState, useEffect} from 'react';
+import { useState, useEffect } from 'react';
 import AnimatedLetters from '../AnimatedLetters';
 import Loader from 'react-loaders';
+import gsap from 'gsap-trial';
+import useInterval from '../../hooks/useInterval';
+import { ECommerceAnimation, EcommerceHome, GraphAnimation, GraphHome, NotesAppHome, NotesAppMarkdown, PrettyMenuAnimation, PrettyMenuIMG, QuizHomePage, QuizResultsPage, SemCoreAnimation, SemCoreHome, StarbucksAnimation, StarbucksHome, TwitterCloneHome, TwitterCloneProfile } from '../../assets/images/project_images';
+// import se from '../../assets/images/project_images/SemcoreHome.PNG'
 const Projects = () => {
     const [letterClass, setLetterClass] = useState('text-animate');
     const [isImage1, setIsImage1] = useState(true)
-
+    const timeline = gsap.timeline();
 
     const myProjects = [
         {
             headline: "Semantic Core",
-            description: "Appending currency sign to a purchase form in your e-commerce site using plain JavaScript.",
-            image1: "https://images.unsplash.com/photo-1672605183243-22fd7c7fd73e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
-            image2: "https://images.unsplash.com/photo-1549989476-69a92fa57c36?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
-            repoURL: "",
-            deploymentURL: "",
+            description: "Visualize relationship between meaning and grammatical gender across 19 languages with this \
+                          full-stack, AI-Powered, National Science Foundation-funded site",
+            image1: SemCoreHome,
+            image2: SemCoreAnimation,
+            repoURL: "https://github.com/EthanAmato/SemanticCoreFrontend",
+            deploymentURL: "https://tinyurl.com/semanticcoreEA",
             isDeployed: true,
         },
         {
             headline: "Fullstack Notetaking App",
-            description: "Appending currency sign to a purchase form in your e-commerce site using plain JavaScript. 2",
-            image1: "https://images.unsplash.com/photo-1672605183243-22fd7c7fd73e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
-            image2: "https://images.unsplash.com/photo-1549989476-69a92fa57c36?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
-            repoURL: "",
-            deploymentURL: "",
+            description: "Built with OAuth2 and Firebase NoSQL Integration, make an account and write as many markdown-compatible \
+                          notes as you please. Also allows for creation of and sorting by custom tags",
+            image1: NotesAppHome,
+            image2: NotesAppMarkdown,
+            repoURL: "https://github.com/EthanAmato/TypeScriptNoteTakerApp",
+            deploymentURL: "https://ethanamato.github.io/TypeScriptNoteTakerApp/",
             isDeployed: true,
         },
         {
             headline: "3D Interactive Plotter",
-            description: "Appending currency sign to a purchase form in your e-commerce site using plain JavaScript. 3",
-            image1: "https://images.unsplash.com/photo-1672605183243-22fd7c7fd73e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
-            image2: "https://images.unsplash.com/photo-1549989476-69a92fa57c36?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60", repoURL: "",
-            deploymentURL: "",
+            description: "Given a Microsoft Excel File, visualize data points in 3D space! Allows options for sizing, titles, \
+                           vectors to custom origins, and download to HTML file for embedding in other sites",
+            image1: GraphHome,
+            image2: GraphAnimation,
+            repoURL: "https://github.com/EthanAmato/3dPlotter",
+            deploymentURL: "https://ethanamato.github.io/3dPlotter/",
             isDeployed: true,
         },
         {
             headline: "Copycat Starbucks Site",
-            description: "Appending currency sign to a purchase form in your e-commerce site using plain JavaScript. 3",
-            image1: "https://images.unsplash.com/photo-1672605183243-22fd7c7fd73e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
-            image2: "https://images.unsplash.com/photo-1549989476-69a92fa57c36?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
-            repoURL: "",
-            deploymentURL: "",
+            description: "A simple, yet beautiful copy of the Holiday Rendition of the Starbucks Website homepage",
+            image1: StarbucksHome,
+            image2: StarbucksAnimation,
+            repoURL: "https://github.com/EthanAmato/Starbucks-Copy",
+            deploymentURL: "https://ethanamato.github.io/Starbucks-Copy/",
             isDeployed: true,
         },
         {
             headline: "Fullstack Twitter Clone",
-            description: "Appending currency sign to a purchase form in your e-commerce site using plain JavaScript. 3",
-            image1: "https://images.unsplash.com/photo-1672605183243-22fd7c7fd73e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
-            image2: "https://images.unsplash.com/photo-1549989476-69a92fa57c36?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
-            deploymentURL: "",
-            isDeployed: true,
+            description: "A Twitter clone complete with working hashtags, authentication, following, and home feed. \
+                          Made with Springboot in Java",
+            image1: TwitterCloneHome,
+            image2: TwitterCloneProfile,
+            repoURL: "https://github.com/EthanAmato/TechTalentTwitter",
+            isDeployed: false,
         },
         {
             headline: "MERN-Stack Quiz App",
-            description: "Appending currency sign to a purchase form in your e-commerce site using plain JavaScript. 3",
-            image1: "https://images.unsplash.com/photo-1672605183243-22fd7c7fd73e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
-            image2: "https://images.unsplash.com/photo-1549989476-69a92fa57c36?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
-            deploymentURL: "",
-            isDeployed: true,
+            description: "MongoDB, ExpressJS, React, Redux, and Node.js project that serves up questions and answers through an API \
+                          allows for accurate scoring and storage of past results by username",
+            image1: QuizHomePage,
+            image2: QuizResultsPage,
+            repoURL: "https://github.com/EthanAmato/PediatricQuizzerClient",
+            isDeployed: false,
         },
         {
             headline: "Responsive Menu Design",
-            description: "Appending currency sign to a purchase form in your e-commerce site using plain JavaScript. 3",
-            image1: "https://images.unsplash.com/photo-1672605183243-22fd7c7fd73e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
-            image2: "https://images.unsplash.com/photo-1549989476-69a92fa57c36?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
-            deploymentURL: "",
+            description: "A beautiful, responsive menu design made only with HTML, CSS, and Javascript",
+            image1: PrettyMenuIMG,
+            image2: PrettyMenuAnimation,
+            repoURL: "https://github.com/EthanAmato/responsive-menu",
+            deploymentURL: "https://ethanamato.github.io/responsive-menu/",
             isDeployed: true,
         },
         {
             headline: "ECommerce Website with Stripe Middleware",
-            description: "Appending currency sign to a purchase form in your e-commerce site using plain JavaScript. 3",
-            image1: "https://images.unsplash.com/photo-1672605183243-22fd7c7fd73e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
-            image2: "https://images.unsplash.com/photo-1549989476-69a92fa57c36?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
-            repoURL: "",
-            deploymentURL: "",
-            isDeployed: true,
+            description: "A Springboot Ecommerce site that uses Stripe middleware to process user transactions.",
+            image1: EcommerceHome,
+            image2: ECommerceAnimation,
+            repoURL: "https://github.com/EthanAmato/EcommerceSite",
+            isDeployed: false,
         },
 
     ]
-    useEffect(() => {
-        const interval = setInterval(() => {
-            if(isImage1) {
-                $(".react-multi-carousel-item .image1, .react-multi-carousel-item .image1-link")
-                .toggleClass("opacityNone")
-                setTimeout(function () {
-                    $(".react-multi-carousel-item .image1, .react-multi-carousel-item .image1-link").toggleClass("displayNone");
-                    $(".react-multi-carousel-item .image2, .react-multi-carousel-item .image2-link").toggleClass("displayNone");
-                }, 1200);
-                setTimeout(function () {
-                    $(".react-multi-carousel-item .image2, .react-multi-carousel-item .image2-link")
-                    .toggleClass("opacityNone")
-                }, 1300);
-            } else {
-                $(".react-multi-carousel-item .image2, .react-multi-carousel-item .image2-link")
-                .toggleClass("opacityNone")
-                setTimeout(function () {
-                    $(".react-multi-carousel-item .image2, .react-multi-carousel-item .image2-link").toggleClass("displayNone");
-                    $(".react-multi-carousel-item .image1, .react-multi-carousel-item .image1-link").toggleClass("displayNone");
-                }, 1200);
-                setTimeout(function () {
-                    $(".react-multi-carousel-item .image1, .react-multi-carousel-item .image1-link")
-                    .toggleClass("opacityNone")
-                }, 1300);
-            }
-            setIsImage1(!isImage1);
-        }, 10000)
-        return () => {
-            clearInterval(interval);
-        };
-    }, [isImage1])
-
 
     useEffect(() => {
         setTimeout(() => {
             setLetterClass('text-animate-hover')
         }, 3000)
     }, [])
+
+
+
+
+    //custom hook that uses dynamic state changes to replay transitioning effect
 
 
     return (
@@ -139,8 +121,8 @@ const Projects = () => {
                         additionalTransfrom={0}
                         arrows
                         draggable={false}
-                        // autoPlay
-                        autoPlaySpeed={3000}
+                        autoPlay
+                        autoPlaySpeed={15000}
                         centerMode={true}
                         className="carousel"
                         containerClass="container-with-dots"
@@ -195,7 +177,9 @@ const Projects = () => {
                                     headline={project.headline}
                                     image1={project.image1}
                                     image2={project.image2}
-                                    isDeployed={true}
+                                    repoURL={project.repoURL}
+                                    deploymentURL={project.deploymentURL}
+                                    isDeployed={project.isDeployed}
                                 />
                             );
                         })}
